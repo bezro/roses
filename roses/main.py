04ee54b022b70f639c2d4979bcc0abdadc4413c4
bezro <bezrogota@gmail.com>
2023-09-22 14:35:16 +0300

    Initial commit

---
+++ roses/main.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+rfc = RandomForestClassifier(n_estimators=25)
+rfc.fit(X_train, y_train)
+y_pred = rfc.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(accuracy)
